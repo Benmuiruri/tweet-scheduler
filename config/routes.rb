d6403d6c2eec04da009129d4ecb33f1ef5b6ed97
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-06 17:06:01 +0300

    Add sign_in routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   #Create New Signup
   post 'sign_up', to: 'registrations#create'
 
+  #Login 
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+
   #Log out user
   delete 'logout', to: 'sessions#destroy'
 end
