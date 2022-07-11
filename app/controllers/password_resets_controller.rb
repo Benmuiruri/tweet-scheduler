553d0bf97500301f3cbda78440160a5510762c60
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-11 09:26:47 +0300

    Create password reset mailer and update password reset create method

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,6 +4,10 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-    
+    @user = User.find_by(email: params[:email])
+    if @user.present?
+      
+    end
+    redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password"
   end
 end
