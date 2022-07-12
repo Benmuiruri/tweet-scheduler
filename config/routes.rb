6e3f72f57adb8ec9741b377086f7c1d0dc3d0933
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-12 09:35:45 +0300

    Add omniauth callback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'main#index'
+  
 
   get 'about-page', to: 'about#index', as: :about
 
   # Passwords
   get 'password', to: 'passwords#edit', as: :edit_password
   patch 'password', to: 'passwords#update'
-
-  # Send out Password reset
-  get 'password/reset', to: 'password_resets#new'
-  post 'password/reset', to: 'password_resets#create'
-
-  # Handle Password reset
-  get 'password/reset/edit', to: 'password_resets#edit'
-  patch 'password/reset/edit', to: 'password_resets#update'
 
   # Sign up
   get 'sign_up', to: 'registrations#new'
@@ -25,4 +17,17 @@ Rails.application.routes.draw do
 
   # Log out user
   delete 'logout', to: 'sessions#destroy'
+
+  # Send out Password reset
+  get 'password/reset', to: 'password_resets#new'
+  post 'password/reset', to: 'password_resets#create'
+
+  # Handle Password reset
+  get 'password/reset/edit', to: 'password_resets#edit'
+  patch 'password/reset/edit', to: 'password_resets#update'
+
+  #Omniauth 
+  get "/auth/twitter/callback", to: "omniauth_callback#twitter"
+
+  root to: 'main#index'
 end
